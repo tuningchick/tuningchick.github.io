bdca9633cc0e6882bfca1f5a7615e5a86106af6d
tuningchick <lyk17002@byui.edu>
2019-02-20 14:32:28 -0500

    updating to begin lesson 7

---
+++ assignments/lesson-07/scripts/currentdate.js
@@ -0,0 +1,25 @@
+// Input: computer entered
+// Process: use .getDate and two arrays to compile the date 
+// Output: date in this format: day, date month, year
+
+function actualDate() {
+    var d = new Date();
+    var dayOfMonth = d.getDate();
+    var year = d.getFullYear();
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"];
+    var month = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+
+    var currentDay = weekday[d.getDay()];
+    var currentMonth = month[d.getMonth()];
+
+document.getElementById("displayDate").innerHTML = currentDay + ", " + dayOfMonth + "  " +
+    currentMonth + ", " + year;
+}
+
+// create toggle function to make the small view hamburger menu work
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}

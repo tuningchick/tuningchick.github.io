d412dc6049397c9d8b5ff140d2ccdff674fc3f0b
tuningchick <lyk17002@byui.edu>
2019-02-20 20:48:48 -0500

    correcting javascript

---
+++ assignments/lesson-07/scripts/windchill.js
@@ -0,0 +1,17 @@
+// Calculate wind chill
+// use two functions to calculate wind chill and return to website
+// output wind chill in degrees F
+function doInputOutput() {
+    //get temp and wind speed from website data
+    var inputTemp = parseFloat(document.getElementById("temperature").value);
+    var inputSpeed = parseFloat(document.getElementById("windSpeed").value);
+    var chill = windChill(inputTemp, inputSpeed);
+    document.getElementById("displayWindChill").innerHTML = "Wind chill: " + chill.toFixed(1)
+        + "&deg";
+}
+
+function windChill(temp, speed) {
+    var f = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16))
+        + (0.4275 * temp * Math.pow(speed, 0.16));
+    return f;
+}

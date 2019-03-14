4e6242737eb1cf5c07a09df68cdce27ffb80b210
tuningchick <lyk17002@byui.edu>
2019-03-14 10:15:54 -0400

    will this current conditions api stuff actually work??

---
+++ assignments/lesson-10/scripts/townweatherapi.js
@@ -0,0 +1,42 @@
+/* thanks, Kimi, for pointing out that I could use the town ID to call the function so that this code will work for each town.
+Sometimes I am a little slow to the draw!! */
+function outputConditions(townID) {
+/* create and open new request */
+
+let weatherRequest = new XMLHttpRequest();
+    weatherRequest.open ("GET", "https://api.openweathermap.org/data/2.5/forecast?id=" + 
+    townID + "&units=imperial&APPID=93f7b5fbca8fc6183352adb88e36039d", true);
+/* send the request */
+weatherRequest.send();
+
+/* get response from server and do something with it */
+
+weatherRequest.onload = function() {
+        let weatherData = JSON.parse(weatherRequest.response);
+        console.log(weatherData);
+        let currentAPIData = weatherData;
+        var span = document.querySelector('#weatherSummary');
+        conditions(currentAPIData);
+
+/* display temp, humidity, wind, current conditions and wind chill in weather summary */
+
+function conditions(jsonObj) {
+    var outputTemp = jsonObj.list[0].main.temp;
+    var outputHumid = jsonObj.list[0].main.humidity;
+    var outputWind = jsonObj.list[0].wind.speed;
+
+    var myConditions = document.createElement('article');
+    var myTemp = document.createElement('p');
+    var myHumidity = document.createElement('p');
+    var myWind = document.createElement('p');
+
+    myTemp.innerHTML = 'Temp: ' + outputTemp + '&deg; F';
+    myHumidity.textcontent = 'Humidity: ' + outputHumid + '%';
+    myWind.innerHTML = 'Wind: ' + outputWind + 'mph';
+
+    myConditions.appendChild(myTemp);
+    myConditions.appendChild(myHumidity);
+    myConditions.appendChild(myWind);
+}
+}
+}

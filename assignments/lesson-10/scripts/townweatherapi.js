df48d161c286aac1616c7b3d23ac934a7d543dab
tuningchick <lyk17002@byui.edu>
2019-03-14 23:07:43 -0400

    confused

--- assignments/lesson-10/scripts/townweatherapi.js
+++ assignments/lesson-10/scripts/townweatherapi.js
@@ -101,9 +101,10 @@ forecastRequest.onload = function() {
         if (timeSearch.search('18:00:00') != -1) {
             temp[time] = document.createElement('td');
             time++;
-            temp[time].innerHTML = forecastData.list[i].main.temp_max + '&deg; F';   
+            temp[time].innerHTML = forecastData.list[i].main.temp_max + '&deg; F';
+            tempRow.appendChild(temp[time]);   
         }
-        tempRow.appendChild(temp[time]);
+        
     }
     table.appendChild(tempRow);       
 }

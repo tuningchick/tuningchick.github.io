bc8f85ce09ed450d7fd8b50414b710a58494a204
tuningchick <lyk17002@byui.edu>
2019-03-14 23:10:16 -0400

    time to ask for help

--- assignments/lesson-10/scripts/townweatherapi.js
+++ assignments/lesson-10/scripts/townweatherapi.js
@@ -101,10 +101,9 @@ forecastRequest.onload = function() {
         if (timeSearch.search('18:00:00') != -1) {
             temp[time] = document.createElement('td');
             time++;
-            temp[time].innerHTML = forecastData.list[i].main.temp_max + '&deg; F';
-            tempRow.appendChild(temp[time]);   
+            temp[time].innerHTML = forecastData.list[i].main.temp_max + '&deg; F';       
         }
-        
+        tempRow.appendChild(temp[time]);
     }
     table.appendChild(tempRow);       
 }

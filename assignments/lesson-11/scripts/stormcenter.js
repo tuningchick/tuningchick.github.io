0b9524b26c71629d5cead12ba53e62b15829374c
tuningchick <lyk17002@byui.edu>
2019-03-24 16:26:33 -0400

    trying minifying index html

---
+++ assignments/lesson-11/scripts/stormcenter.js
@@ -0,0 +1,4 @@
+// Cause the value of the slider (range element) to increase or decrease between 1 and 10
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
---
+++ assignments/lesson-11/scripts/weatherapi.js
@@ -0,0 +1,29 @@
+
+/* create and open new request */
+
+let weatherRequest = new XMLHttpRequest();
+    weatherRequest.open ("GET", "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=93f7b5fbca8fc6183352adb88e36039d", true);
+/* send the request */
+weatherRequest.send();
+
+/* get response from server and do something with it */
+
+weatherRequest.onload = function() {
+        let weatherData = JSON.parse(weatherRequest.response);
+        console.log(weatherData);
+        let currentTemp = weatherData;
+        var span = document.querySelector('#currentTemp');
+        Temperature(currentTemp);
+
+/* display current temperature on test page, thanks Sis. Campbell for helping me streamline this */
+
+function Temperature(jsonObj) {
+    var outputMain = jsonObj.list[0].main.temp;
+    console.log(outputMain);
+    var myP = document.createElement('p');
+
+    myP.textcontent = outputMain;
+
+    span.innerHTML = outputMain;
+}
+}

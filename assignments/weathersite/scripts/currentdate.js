8a564327a972a0b9d70693883122ee28d8d377b2
tuningchick <lyk17002@byui.edu>
2019-03-24 18:38:23 -0400

    minification

--- assignments/weathersite/scripts/currentdate.js
+++ assignments/weathersite/scripts/currentdate.js
@@ -1,1 +1,6 @@
-function actualDate(){var e=new Date,a=e.getDate(),t=e.getFullYear(),n=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][e.getDay()],r=["January","February","March","April","May","June","July","August","September","October","November","December"][e.getMonth()];document.getElementById("displayDate").innerHTML=n+", "+a+"  "+r+", "+t}function toggleMenu(){document.getElementById("primaryNav").classList.toggle("hide")}
+function actualDate(){var e=new Date,a=e.getDate(),t=e.getFullYear(),n=
+    ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][e.getDay()],r=
+    ["January","February","March","April","May","June","July","August","September","October",
+    "November","December"][e.getMonth()];document.getElementById("displayDate").innerHTML=n+",
+     "+a+"  "+r+", "+t}function toggleMenu(){document.getElementById("primaryNav")
+     .classList.toggle("hide")}

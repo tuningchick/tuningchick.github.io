cba68211f76cec048fd0c1bee9487157ce60244e
tuningchick <lyk17002@byui.edu>
2019-03-24 17:27:23 -0400

    events js compressed

--- assignments/weathersite/scripts/events.js
+++ assignments/weathersite/scripts/events.js
@@ -1,41 +1,5 @@
-function runEvents(town) {
-var section = document.querySelector('#upcoming');
-var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-/* create and open new request */
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-/* set response type to JSON */
-request.responseType = 'json';
-request.send();
-/* get response from server and deal with it */
-request.onload = function() {
-    var upcoming = request.response;
-    eventData(upcoming);
-
-
-/* create and display upcoming events */
-function eventData(jsonObj) {
-    var townName = jsonObj['towns'];
-    /* look for right town */
-    for (var i = 0; i < townName.length; i++) {
-        if (townName[i].name == town) {
-            var event = townName[i].events;
-            
-            var eventList = document.createElement('ul');
-
-            /* loop to determine how many events and add them to list */
-            for (var e = 0; e < event.length; e++) {
-                var myEvent = document.createElement('li');
-
-                myEvent.innerHTML = event[e];
-                eventList.appendChild(myEvent);
-            }
-
-            /* take list items and add to section container */
-            section.appendChild(eventList);
-        }
-           
-    }
-}
-} 
-}
+function runEvents(u){var s=document.querySelector("#upcoming"),e=new XMLHttpRequest;
+e.open("GET","https://byui-cit230.github.io/weather/data/towndata.json"),e.responseType="json",
+e.send(),e.onload=function(){!function(e){for(var n=e.towns,t=0;t<n.length;t++)if(n[t].name==u)
+{for(var o=n[t].events,a=document.createElement("ul"),r=0;r<o.length;r++)
+{var i=document.createElement("li");i.innerHTML=o[r],a.appendChild(i)}s.appendChild(a)}}(e.response)}}

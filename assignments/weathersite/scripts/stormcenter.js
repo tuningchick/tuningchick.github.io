9b7c9236dba2d4c75d4688bf787db1689a731923
tuningchick <lyk17002@byui.edu>
2019-03-24 18:50:11 -0400

    minimize stormcenter js

--- assignments/weathersite/scripts/stormcenter.js
+++ assignments/weathersite/scripts/stormcenter.js
@@ -1,4 +1,1 @@
-// Cause the value of the slider (range element) to increase or decrease between 1 and 10
-function adjustRating(rating) {
-    document.getElementById("ratingvalue").innerHTML = rating;
-}
+function adjustRating(n){document.getElementById("ratingvalue").innerHTML=n}

c3f89ce8569381767e5ae4a82916621f90a8eb9b
tuningchick <lyk17002@byui.edu>
2019-04-05 15:41:23 -0400

    fixing js date for weathersite

--- assignments/weathersite/scripts/currentdate.js
+++ assignments/weathersite/scripts/currentdate.js
@@ -1,6 +1,25 @@
-function actualDate(){var e=new Date,a=e.getDate(),t=e.getFullYear(),n=
-    ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][e.getDay()],r=
-    ["January","February","March","April","May","June","July","August","September","October",
-    "November","December"][e.getMonth()];document.getElementById("displayDate").innerHTML=n+",
-     "+a+"  "+r+", "+t}function toggleMenu(){document.getElementById("primaryNav")
-     .classList.toggle("hide")}
+// Input: computer entered
+// Process: use .getDate and two arrays to compile the date 
+// Output: date in this format: day, date month, year
+
+function actualDate() {
+    var d = new Date();
+    var dayOfMonth = d.getDate();
+    var year = d.getFullYear();
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday"];
+    var month = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+
+    var currentDay = weekday[d.getDay()];
+    var currentMonth = month[d.getMonth()];
+
+document.getElementById("displayDate").innerHTML = currentDay + ", " + dayOfMonth + "  " +
+    currentMonth + ", " + year;
+}
+
+// create toggle function to make the small view hamburger menu work
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
